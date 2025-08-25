20a6e2c7f0d4d0ed7d4f7d9b5436c8ef5c86db78
Jordan Lenon <jordanlenon@gmail.com>
2025-08-25 08:14:46 -0300

    feat: redirect home to order 1

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/orders/:id', component: () => import('@/components/orders/OrderDetails.vue') },
+    {
+      path: '/',
+      redirect: '/orders/1'
+    },
+    {
+      path: '/orders/:id',
+      component: () => import('@/components/orders/OrderDetails.vue')
+    }
   ],
 })
 
